4ab348ab5272abfe1936b908055be0cb336f0866
Trevor Low <tlow94@uw.edu>
2020-04-27 16:13:15 +0000

    Automatically backed up by Learn

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,13 +1,14 @@
 require 'pry'
 class Startup
-attr_accessor :name
+attr_accessor :name, :domain
 attr_reader :founder
-def initialize (name, founder)
+def initialize (name, founder, domain)
   @name = name
   @founder = founder
+  @domain = domain
 end
 end
 
-"Trevor" = Startup.new("Trevor")
+"Trevor" = Startup.new("Google", "Man", "www")
 
 binding.pry
